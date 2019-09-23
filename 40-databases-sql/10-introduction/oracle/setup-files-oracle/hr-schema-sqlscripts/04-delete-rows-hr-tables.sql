e9c12cef3611c5d141e553264e448bbb0337ad4d
reinouvb <reinout@switchfully.com>
2019-09-23 17:59:32 +0200

    adjustments to SQL scripts HR schema

---
+++ 40-databases-sql/10-introduction/oracle/setup-files-oracle/hr-schema-sqlscripts/04-delete-rows-hr-tables.sql
@@ -0,0 +1,7 @@
+delete from hr.locations;
+delete from hr.countries;
+delete from hr.regions;
+delete from hr.departments;
+delete from hr.jobs;
+delete from hr.employees;
+delete from hr.job_history;
