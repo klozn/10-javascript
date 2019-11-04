9a0db32709f52f039daacb5f1d3759f4b8b16df7
reinouvb <reinout@switchfully.com>
2019-11-04 16:10:41 +0100

    adjustments to SQL scripts HR schema: merge conflict opgelost

--- 40-databases-sql/10-introduction/oracle/setup-files-oracle/hr-schema-sqlscripts/00-general.sql
+++ 40-databases-sql/10-introduction/oracle/setup-files-oracle/hr-schema-sqlscripts/00-general.sql
@@ -12,21 +12,20 @@ DECLARE
     current_username VARCHAR2(12);
     current_password VARCHAR2(12);
     user_id number (10);
-    number_of_users number (3):=2;
+    number_of_users number (4):=50;
 BEGIN
     for user_id in 1..number_of_users
 
     LOOP
 
-    current_username :='HR_STUDENT'||user_id;
-    current_password :='HR_STUDENT'||user_id;
+    current_username :='HRSTUDENT'||user_id;
+    current_password :='HRSTUDENT'||user_id;
 
     EXECUTE IMMEDIATE 'CREATE USER '||current_username||' IDENTIFIED BY '||current_password;
     EXECUTE IMMEDIATE 'GRANT CREATE SESSION TO '||current_username;
     EXECUTE IMMEDIATE 'GRANT CREATE TABLE TO '||current_username;
     EXECUTE IMMEDIATE 'GRANT CREATE SEQUENCE TO '||current_username;
     EXECUTE IMMEDIATE 'GRANT UNLIMITED TABLESPACE  TO '||current_username;
-    EXECUTE IMMEDIATE 'GRANT SEQUENCE TABLESPACE  TO '||current_username;
 
 ---------------------------------------------------------------
 
