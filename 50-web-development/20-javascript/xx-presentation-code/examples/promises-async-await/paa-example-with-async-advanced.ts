e652c68d48106396f3a204b17200c503172e663d
nielsdelestinne <nielsdelestinne@gmail.com>
2019-11-20 20:56:44 +0100

    Examples added on asyn-await

---
+++ 50-web-development/20-javascript/xx-presentation-code/examples/promises-async-await/paa-example-with-async-advanced.ts
@@ -0,0 +1,37 @@
+/**
+ * 1. Compile using: tsc --target es2015 paa-example-with-async-advanced.ts
+ * 2. Execute using: node paa-example-with-async-advanced.js
+ * 3. More examples / documentation on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+ */
+
+function resolveAfter4Seconds() {
+    console.log("--> starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function() {
+            resolve("Result for Slow received");
+            console.log("<-- slow promise is done");
+        }, 4000);
+    });
+};
+
+function resolveAfter1Second() {
+    console.log("--> starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function() {
+            resolve("Result for Fast received");
+            console.log("<-- fast promise is done");
+        }, 1000);
+    });
+};
+
+async function start() {
+    console.log('Start!');
+    const slow = resolveAfter4Seconds(); // starts timer immediately
+    const fast = resolveAfter1Second(); // starts timer immediately
+
+    console.log('At position [A]');// [A] -> Execution gets here almost instantly
+    console.log(await slow); // [B] -> this statements runs 4 seconds after [A]
+    console.log(await fast); // [C] -> this statement runs 4 seconds after [A], but immediately after [B], since our fast promise is already done 3 seconds (but [C] has to wait for statement [B] to be executed).
+}
+
+start();
---
+++ 50-web-development/20-javascript/xx-presentation-code/examples/promises-async-await/paa-example-without-async.ts
@@ -0,0 +1,16 @@
+/**
+ * 1. Compile using: tsc --target es2015 paa-example-without-async.ts
+ * 2. Execute using: node paa-example-without-async.js
+ */
+
+function doStuff() {
+    console.log('Stuck in the middle!');
+}
+
+/*
+* Synchronous code. Each of these 3 statements is executed one after the other (sequential).
+* This means each statement has to wait for the previous one to finish executing.
+*/
+console.log('Before');
+doStuff();
+console.log('After');
