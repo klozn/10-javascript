0ac9011a30ad412385eeb8b5d30f756960fdfee6
Klaas Devits <klaas.devits@gmail.com>
2021-11-25 09:49:31 +0100

    10-variables-and-values: basic codelabs done

---
+++ 50-web-development/20-javascript-typescript/10-javascript/10-variables-and-values/10-basic/codelab04/index.js
@@ -0,0 +1,11 @@
+let foo = 5;
+
+console.log(typeof foo);
+
+let bar = typeof foo;
+console.log(typeof bar)
+
+{
+    let foo;
+    console.log(typeof foo)
+}
---
+++ 50-web-development/20-javascript-typescript/10-javascript/10-variables-and-values/10-basic/codelab05/index.js
@@ -0,0 +1,6 @@
+const foo = 5;
+
+let bar = 3;
+
+foo = 7;
+bar = 9;
